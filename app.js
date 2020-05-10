a7d5ff1b5e2122bfe6796da7402abfcff7ed0a03
bonino97 <juanbonino97@gmail.com>
2020-05-10 01:12:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ app.use(bodyParser.json())
 //                              CORS
 ///#############################################################################
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // DOMINIO PERMITIDO PARA INTERACTUAR CON EL SVR
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
