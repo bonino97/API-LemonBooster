35510b1b8cd1b4bed9d3745d015cad149264d860
bonino97 <juanbonino97@gmail.com>
2020-05-10 00:42:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(bodyParser.json())
 //                              CORS
 ///#############################################################################
 
-app.use(function(req, res, next) {
+app.all(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // DOMINIO PERMITIDO PARA INTERACTUAR CON EL SVR
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
-  });
+});
 
 
 
